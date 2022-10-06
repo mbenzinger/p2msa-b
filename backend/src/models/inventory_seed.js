7482fa5163160de6d1c792fc417160a4688bece0
Mason Benzinger <mason.benzinger@gmail.com>
2022-10-06 11:56:05 -0500

    added seed file

---
+++ backend/src/models/inventory_seed.js
@@ -0,0 +1,58 @@
+module.exports = [
+    {
+        company: 'Keen',
+        name: 'Atlanta',
+        type: 'mens',
+        size: '8',
+        sku: '1023218'
+    },
+    {
+        company: 'Keen',
+        name: 'Braddock',
+        type: 'mens',
+        size: '9',
+        sku: '1020162'
+    },
+    {
+        company: 'Keen',
+        name: 'Pittsburgh',
+        type: 'mens',
+        size: '9.5',
+        sku: '1009709'
+    },
+    {
+        company: 'Keen',
+        name: 'Detroit',
+        type: 'mens',
+        size: '10',
+        sku: '1020039'
+    },
+    {
+        company: 'Keen',
+        name: 'PTC 5/0',
+        type: 'mens',
+        size: '10.5',
+        sku: '1006983'
+    },
+    {
+        company: 'Keen',
+        name: 'Birmingham',
+        type: 'mens',
+        size: '10.5',
+        sku: '1026360'
+    },
+    {
+        company: 'Keen',
+        name: 'Flint',
+        type: 'mens',
+        size: '8',
+        sku: '1023268'
+    },
+    {
+        company: 'Keen',
+        name: 'Vista',
+        type: 'mens',
+        size: '10',
+        sku: '1026371'
+    },
+]
